fe1838d10d78b7cd9ee555a74568419434f2546f
Joseph Nolan <josephn0624@gmail.com>
2017-05-15 09:50:31 -0500

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import Axios from 'axios';
+
+const API_KEY = '5b32005487b8122257abed094a618c11';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${ API_KEY }`;
+
+// good practice to export actions types as variables to avoid bugs
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather( cityName ) {
+    const url = `${ ROOT_URL }&q=${ cityName },us`;
+    const request = Axios.get( url );
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Searchbar from '../containers/Searchbar'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Searchbar />
+      </div>
+      
+    );
+  }
+}
